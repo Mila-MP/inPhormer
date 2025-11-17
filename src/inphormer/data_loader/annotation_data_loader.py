4507ddd59c320ba55def344467322a0294fa63ba
CaraEH <cara.j.holt@gmail.com>
2025-11-17 10:40:29 +0100

    updated AnnotationDataLoader so that the 5k training set can be accessed

--- src/inphormer/data_loader/annotation_data_loader.py
+++ src/inphormer/data_loader/annotation_data_loader.py
@@ -11,22 +11,30 @@ class AnnotationDataLoader(object):
 
     def get_train(self):
         """
-        Rerturn the pre-splited training data
+        Return the post-splited training data
         """
         train_path = os.path.join(self._config.output_dir, self._config.train_filename)
         return pd.read_csv(train_path, sep="\t")
 
     def get_validation(self):
         """
-        Rerturn the pre-splited val data
+        Return the post-splited val data
         """
         val_path = os.path.join(self._config.output_dir, self._config.val_filename)
         return pd.read_csv(val_path, sep="\t")
 
     def get_test(self):
         """
-        Rerturn the pre-splited test data
+        Return the post-splited test data
         """
 
         test_path = os.path.join(self._config.output_dir, self._config.test_filename)
         return pd.read_csv(test_path, sep="\t")
+    
+    def get_train_all(self):
+        """
+        Return the entire 5k data set
+        """
+        all_path = os.path.join(self._config.output_dir, self._config.train_all_filename)
+        return pd.read_csv(all_path, sep="\t")
+
--- src/inphormer/data_loader/config.py
+++ src/inphormer/data_loader/config.py
@@ -9,3 +9,4 @@ class DataLoaderConfig(object):
     train_filename = r"train_split.tsv"
     val_filename = r"val_split.tsv"
     test_filename = r"test_split.tsv"
+    train_all_filename = r"train_all.tsv"
