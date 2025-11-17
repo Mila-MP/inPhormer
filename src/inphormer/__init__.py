c353bcd665c8af3138364ed52419cb22d0b4ce3d
Elon <Elon.gawimatok@student.kuleuven.be>
2025-11-17 16:50:13 +0100

    v0.1.2

--- src/inphormer/__init__.py
+++ src/inphormer/__init__.py
@@ -1,3 +1,4 @@
 from .models.embedder_with_classification_head import EmbedderClassifier
 from .models.llm.classifier import BERTBasedModel
 from .models.tf_idf import TFIDFClassifier
+from .data_loader.annotation_data_loader import AnnotationDataLoader
